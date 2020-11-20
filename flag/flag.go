f61584c9346b526a661dd14b1c6f190f7e8150c3
root <root@DESKTOP-BFJMH29.localdomain>
2020-11-20 18:54:27 +0800

    add "nginx example" through flag module pack original nginx command program

--- flag/flag.go
+++ flag/flag.go
@@ -3,38 +3,67 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 )
 
-// 定义命令行参数对应的变量，这三个变量都是指针类型
-var cliName = flag.String("name", "nick", "Input Your Name")
-var cliAge = flag.Int("age", 28, "Input Your Age")
-var cliGender = flag.String("gender", "male", "Input Your Gender")
+//
+var (
+	h bool
 
-// 定义一个值类型的命令行参数变量，在 Init() 函数中对其初始化
-// 因此，命令行参数对应变量的定义和初始化是可以分开的
-var cliFlag int
+	// V is currnet version's value.
+	v, V bool
+	// T is current unit test's value.
+	t, T bool
+	q    *bool
 
-// Init cliFlag argument by yourself.
-func Init() {
-	flag.IntVar(&cliFlag, "flagname", 1234, "Just for demo")
+	s string
+	p string
+	c string
+	g string
+
+	n int64
+)
+
+func init() {
+	flag.BoolVar(&h, "h", false, "this help")
+	flag.BoolVar(&v, "v", false, "show version and exit")
+	flag.BoolVar(&V, "V", false, "show version and configure options then exit")
+	flag.BoolVar(&t, "t", false, "test configuration and exit")
+	flag.BoolVar(&T, "T", false, "test configuration,dump it and exit")
+
+	//另一中绑定方式
+	q = flag.Bool("q", false, "suppress non-error messages during configuration testing")
+	//注意’signal‘,默认是 -S string 有了`signal`之后，变为-s signal
+	flag.StringVar(&s, "s", "", "send `signal` to a master")
+	flag.StringVar(&p, "p", "/usr/local/nginx", "set configuration `file`")
+	flag.StringVar(&c, "c", "conf/nginx.conf", "set confiruration `file`")
+	flag.StringVar(&g, "g", "conf/nginx.conf", "set global `directives` out of configuraton file")
+
+	flag.Int64Var(&n, "n", 0, "set `number`")
+	//改变默认的usage
+	flag.Usage = Usage
 }
 
 func main() {
-	// 初始化变量 cliFlag
-	Init()
-	// 把用户传递的命令行参数解析为对应变量的值
 	flag.Parse()
-
-	// flag.Args() 函数返回没有被解析的命令行参数
-	// func NArg() 函数返回没有被解析的命令行参数的个数
-	fmt.Printf("args=%s, num=%d\n", flag.Args(), flag.NArg())
-	for i := 0; i != flag.NArg(); i++ {
-		fmt.Printf("arg[%d]=%s\n", i, flag.Arg(i))
+	if h {
+		flag.Usage()
 	}
+	if len(s) != 0 {
+		fmt.Println(s)
+	}
+	if n != 0 {
+		fmt.Println(n)
+	}
+	if flag.NArg() == 0 {
+		flag.Usage()
+	}
+}
 
-	// 输出命令行参数
-	fmt.Println("name=", *cliName)
-	fmt.Println("age=", *cliAge)
-	fmt.Println("gender=", *cliGender)
-	fmt.Println("flagname=", cliFlag)
+// Usage function will show currrent tool's use method.
+func Usage() {
+	fmt.Fprintf(os.Stderr, `nginx version: nginx/1.10.0 Usage:nginx [-hvVtTq] [-n number] [-s signal] [-c filename] [-p prefix] [-g directives]
+Options:
+`)
+	flag.PrintDefaults()
 }
